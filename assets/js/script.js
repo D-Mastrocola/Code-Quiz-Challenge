c884cfb21b8c1ebdfbf3bc2822be7a09664d83b5
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-01 15:59:41 -0400

    you can go through the entire quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,242 @@
-let startButton = document.querySelector('#start-quiz-btn');
-let mainElement = document.querySelector('main');
-let questionCounter = -1;
 let questions = [
-    { 
-        question: "Is this a question?",
-        responses: ['Yes', 'No'],
-        answer: 'Yes'
-    }
+  {
+    question: "What does HTML stand for?",
+    responses: [
+      "Hamburger Text McDonalds Large",
+      "How To Meet Ladies",
+      "Hyper Text Markup Language",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "What does CSS stand for?",
+    responses: [
+      "Counter Strike - Source",
+      "Chicken Sandwich Supreme",
+      "Cascading Style Sheets",
+    ],
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question:
+      "Making a website work on all screen sizes is called ______________",
+    responses: [
+      "Responsive Design",
+      "Unresponsive Design",
+      "Backend Development",
+      "Neural Networks",
+    ],
+    answer: "Responsive Design",
+  },
+  {
+    question: "What is an API?",
+    responses: [
+      "Object.freeze(num);",
+      "num.mutable = false;",
+      "Application Programming Interface",
+    ],
+    answer: "Application Programming Interface",
+  },
+  {
+    question:
+      "Using the const keyword to declare a variable makes the variable immutable",
+    responses: ["True", "False"],
+    answer: "False",
+  },
+  {
+    question: "Primitive Types are immutable",
+    responses: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question:
+      "Which line of code will change (var myVar = 'Hello World';) so that console.log(myVar) will return 'HELLO WORLD'",
+    responses: [
+      "myVar.toUpperCase();",
+      "var myVarUpperCase = myVar.toUpperCase();",
+      "var myVar = 'hello world'",
+    ],
+    answer: "var myVarUpperCase = myVar.toUpperCase();",
+  },
 ];
+
+let startButton = document.querySelector("#start-quiz-btn");
+let mainElement = document.querySelector("main");
+
+let score = 0;
+const MAX_SCORE = questions.length;
+let time = 30;
+
 let endQuiz = () => {
-    console.log('end quiz')
-}
+  let endElements = document.createElement("div");
+  endElements.className = "end-container";
+
+  let endTitle = document.createElement("h2");
+  endTitle.textContent = "Score: " + score + "/" + MAX_SCORE;
+
+  endElements.appendChild(endTitle);
+
+  let endText = document.createElement("p");
+  endText.textContent =
+    "You scored: " + Math.round((score / MAX_SCORE) * 100) + "%";
+  endElements.appendChild(endText);
+
+  mainElement.appendChild(endElements);
+};
+
+let correct = () => {
+  score++;
+  time += 10;
+};
+let incorrect = () => (time -= 10);
+
+let currentQuestion;
+
+let checkAnswer = (event) => {
+  let isRight = false;
+  event.preventDefault();
+
+  let radioButtonElements = document.querySelectorAll(
+    ".question-container input"
+  );
+  console.log(radioButtonElements);
+  for (let i = 0; i < radioButtonElements.length; i++) {
+    if (radioButtonElements[i].checked) {
+      if (
+        document.querySelector("#response-label-" + i).textContent ===
+        currentQuestion.answer
+      ) {
+        isRight = true;
+        break;
+      }
+    }
+  }
+  if (isRight) correct();
+  else incorrect();
+
+  let questionElements = document.querySelector(".question-container");
+
+  anime.timeline({}).add({
+    targets: questionElements,
+    opacity: 0,
+    duration: 500,
+    easing: "linear",
+    update: function (anim) {
+      if (anim.progress >= 100) {
+        //Waits for animation to complete then removes elements from the DOM
+        questionElements.remove();
+        if (questions.length > 0) nextQuestion();
+        else endQuiz();
+      }
+    },
+  });
+};
 let nextQuestion = () => {
-    questionCounter++;
-    if(questionCounter >= questions.length) {
-        endQuiz();
-    }
-    let questionElements = document.createElement('div');
-    questionElements.className = 'question-container';
+  if (questions.length === 0) {
+    endQuiz();
+    return false;
+  }
+  let questionIndex = Math.floor(Math.random() * questions.length);
+  currentQuestion = questions[questionIndex];
+  let questionElements = document.createElement("div");
+  questionElements.className = "question-container";
 
-    let questionText = document.createElement('h3');
-    questionText.textContent = questions[questionCounter].question;
-    questionElements.appendChild(questionText);
+  let questionText = document.createElement("h3");
+  questionText.textContent = currentQuestion.question;
+  questionElements.appendChild(questionText);
 
-    let questionForm = document.createElement('form');
-    for(let i = 0; i < questions[questionCounter].responses.length; i++) {
-        let response = document.createElement('div');
-        response.className ='response';
+  let questionForm = document.createElement("form");
+  for (let i = 0; i < currentQuestion.responses.length; i++) {
+    let response = document.createElement("div");
+    response.className = "response";
 
-        let input = document.createElement('input');
-        input.setAttribute('type', 'radio');
-        input.setAttribute('name', 'response');
-        input.setAttribute('id', 'question-' + i);
-        input.setAttribute('question-id', i)
+    let input = document.createElement("input");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "response");
+    input.setAttribute("id", "response-" + i);
 
+    response.appendChild(input);
 
-        response.appendChild(input);
+    let label = document.createElement("label");
+    label.setAttribute("for", "response-" + i);
+    label.id = "response-label-" + i;
+    label.textContent = currentQuestion.responses[i];
+    response.appendChild(label);
 
-        let label = document.createElement('label');
-        label.setAttribute('for', 'question-' + i)
-        label.textContent = questions[questionCounter].responses[i];
+    questionForm.appendChild(response);
+  }
 
-        response.appendChild(label); 
-        questionForm.appendChild(response);     
-    }
-    questionElements.appendChild(questionForm);
-    mainElement.appendChild(questionElements);
-}
+  let submitButton = document.createElement("button");
+
+  questions.splice(questionIndex, 1);
+  submitButton.textContent = "Next Question";
+
+  if (questions.length === 0) submitButton.textContent = "Finish";
+
+  submitButton.className = "question-submit";
+  submitButton.type = "submit";
+  questionForm.addEventListener("submit", checkAnswer);
+
+  questionForm.appendChild(submitButton);
+
+  questionElements.appendChild(questionForm);
+  mainElement.appendChild(questionElements);
+};
 let startQuiz = () => {
-    let landingElements = document.querySelector('.text-container');
+  let landingElements = document.querySelector(".text-container");
 
-    let loadingElements = document.createElement('div');
-    loadingElements.className = 'loading-container';
+  let loadingElements = document.createElement("div");
+  loadingElements.className = "loading-container";
 
-    let loadingText = document.createElement('p');
-    loadingText.textContent = 'Loading: 0%';
+  let loadingText = document.createElement("p");
+  loadingText.textContent = "Loading: 0%";
 
-    let loadingBar = document.createElement('div')
-    loadingBar.className = 'loading-bar';
+  let loadingBar = document.createElement("div");
+  loadingBar.className = "loading-bar";
 
-    loadingElements.appendChild(loadingText);
-    loadingElements.appendChild(loadingBar);
+  loadingElements.appendChild(loadingText);
+  loadingElements.appendChild(loadingBar);
 
-    anime.timeline({})
+  anime
+    .timeline({})
     .add({
-        targets: landingElements,
-        opacity: 0,
-        duration: 1000,
-        update: function(anim) {
-            if(anim.progress == 100) {
-                //Waits for animation to complete then removes elements from the DOM
-                landingElements.remove();
-                mainElement.appendChild(loadingElements);
-            }
+      targets: landingElements,
+      opacity: 0,
+      duration: 1000,
+      update: function (anim) {
+        if (anim.progress == 100) {
+          //Waits for animation to complete then removes elements from the DOM
+          landingElements.remove();
+          mainElement.appendChild(loadingElements);
         }
-    }).add({
-        targets: loadingElements,
-        opacity: 1,
-        duration: 1000
-    }).add({
-        targets: loadingBar,
-        width: '100%',
-        duration: 2000,
-        easing: 'easeOutExpo',
-        update: function(anim) {
-            loadingText.textContent = "Loading: " + Math.round(anim.progress) + "%";
+      },
+    })
+    .add({
+      targets: loadingElements,
+      opacity: 1,
+      duration: 1000,
+    })
+    .add({
+      targets: loadingBar,
+      width: "100%",
+      duration: 2000,
+      easing: "easeOutExpo",
+      update: function (anim) {
+        loadingText.textContent = "Loading: " + Math.round(anim.progress) + "%";
+      },
+    })
+    .add({
+      targets: loadingElements,
+      opacity: 0,
+      duration: 1000,
+      update: function (anim) {
+        if (anim.progress >= 100) {
+          //Waits for animation to complete then removes elements from the DOM
+          loadingElements.remove();
+          nextQuestion();
         }
-    }).add({
-        targets: loadingElements,
-        opacity: 0,
-        duration: 1000,
-        update: function(anim) {
-            if(anim.progress >= 100) {
-                //Waits for animation to complete then removes elements from the DOM
-                loadingElements.remove();
-                nextQuestion();
-            }
-        }
+      },
     });
-}
+};
 
-
-
-startButton.addEventListener('click', startQuiz);
+startButton.addEventListener("click", startQuiz);
